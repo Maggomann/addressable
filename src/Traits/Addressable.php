f4b19006d9609fd72bfe52e06c643877d9384712
Maggomann <marco.ehrt@googlemail.com>
2022-11-01 16:14:17 +0100

    wip: add relation address

--- src/Traits/Addressable.php
+++ src/Traits/Addressable.php
@@ -3,6 +3,7 @@
 namespace Maggomann\LaravelAddressable\Traits;
 
 use Illuminate\Database\Eloquent\Relations\MorphMany;
+use Illuminate\Database\Eloquent\Relations\MorphOne;
 use Maggomann\LaravelAddressable\Models\Address;
 
 trait Addressable
@@ -10,5 +11,10 @@ trait Addressable
     public function addresses(): MorphMany
     {
         return $this->morphMany(Address::class, 'addressable', 'addressable_type', 'addressable_id');
+    }
+
+    public function address(): MorphOne
+    {
+        return $this->morphOne(Address::class, 'addressable', 'addressable_type', 'addressable_id');
     }
 }
