90b9251f252bbd337f9bf761b62847baafb56756
Maggomann <marco.ehrt@googlemail.com>
2022-11-06 12:19:41 +0100

    refactor: refactoring with phpstan

--- src/Models/Address.php
+++ src/Models/Address.php
@@ -6,9 +6,26 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
-/**
+/** 
+ * @property int|null $gender_id 
+ * @property int|null $category_id
  * @property int $addressable_id
  * @property string $addressable_type
+ * @property string|null $first_name
+ * @property string|null $last_name
+ * @property string|null $name
+ * @property string|null $street_address
+ * @property string|null $street_addition
+ * @property string|null $postal_code
+ * @property string|null $city
+ * @property string|null $country_code
+ * @property string|null $state
+ * @property string|null $company
+ * @property string|null $job_title
+ * @property string|null $latitude
+ * @property string|null $longitude
+ * @property int|bool $is_preferred 
+ * @property int|bool $is_main 
  * @property \Illuminate\Support\Carbon|null $created_at
  * @property \Illuminate\Support\Carbon|null $updated_at
  * @property \Illuminate\Support\Carbon|null $deleted_at
